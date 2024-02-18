634310fbd807a4fd6293608fa3a0fd4493704eb5
arunchandra23 <arunboini999@gmail.com>
2024-02-18 19:07:39 +0530

    mod vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "tailwindcss";
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || '/deploy_react_app_github_pages_vercel',
   css: {
     postcss: {
       plugins: [tailwindcss()],
